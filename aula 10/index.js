3265be76f18d1691806b71efe6f9353ab92f94d7
diegoyun <diegoyun22@gmail.com>
2024-03-20 15:46:58 -0300

    aulas JS

---
+++ aula 10/index.js
@@ -0,0 +1,10 @@
+let nome = "Diego"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira seu nome:")
+let confirmação = window.confirm("Confirma essa ação?")
+console.log(confirmação)
